2319f0af50dca403aeb656181e13399f5f8d24ab
Kiy Hong Tang <kiyhong@gmail.com>
2014-06-18 18:15:45 +0100

    Added test for converting 1 to I

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -5,4 +5,9 @@ describe "RomanNumerals" do
 	it "can create a new instance of RomanNumeral" do
 		r = RomanNumerals.new
 	end
+
+	it "can convert 1 to I" do
+		r = RomanNumerals.new
+    expect(r.convert(1)).to eq("I") 
+  end
 end
