29a80b2447dd3c6cdb9bd9acea5cdb6de80bdfe8
Kiy Hong Tang <kiyhong@gmail.com>
2014-06-19 09:55:37 +0100

    Added test for converting 5 to V

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,6 +2,7 @@ require "roman_numerals"
 
 describe "RomanNumerals" do
 
+	let(:r) {RomanNumerals.new}
 	it "can create a new instance of RomanNumeral" do
 		r = RomanNumerals.new
 	end
@@ -9,5 +10,9 @@ describe "RomanNumerals" do
 	it "can convert 1 to I" do
 		r = RomanNumerals.new
     expect(r.convert(1)).to eq("I") 
+  end
+
+   it "can convert 5 to V" do
+  	expect(r.convert(5)).to eq("V")
   end
 end
