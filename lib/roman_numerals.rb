8a0fa91e8b4055ec8c345b0a36384ad8b47d14cf
Kiy Hong Tang <kiyhong@gmail.com>
2014-06-20 22:11:20 +0100

    Refactored class RomanNumerals

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,13 +1,24 @@
 class RomanNumerals
   
-  NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 
-                400 => "CD", 100 => "C", 90 => "XC", 
-                50 => "L", 40 => "XL", 10 => "X", 
-                9 => "IX", 5 => "V", 
-                4 => "IV", 1 => "I"}
+  NUMERALS = {
+    1000 => "M", 
+    900 => "CM", 
+    500 => "D", 
+    400 => "CD", 
+    100 => "C", 
+    90 => "XC", 
+    50 => "L", 
+    40 => "XL", 
+    10 => "X", 
+    9 => "IX", 
+    5 => "V", 
+    4 => "IV", 
+    1 => "I",
+  }
 
   def convert(arabic_number)
     result = ""
+    
     NUMERALS.each do |arabic, roman|
       (arabic_number/arabic).times { 
         result << roman; 
